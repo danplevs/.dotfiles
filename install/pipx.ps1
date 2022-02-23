1f3449ea8d0d18e364fee99100c13e2bc4c4af8f
danplevs <daniel.re4@gmail.com>
2022-02-22 23:47:27 -0300

    add pipx packages for py projects

--- install/pipx.ps1
+++ install/pipx.ps1
@@ -3,7 +3,7 @@ Set-Location $env:USERPROFILE/AppData/Roaming/Python/Python310/Scripts
 ./pipx ensurepath
 
 # Install packages
-$Packages = 'black', 'ipython', 'poetry', 'pytest'
+$Packages = 'blue', 'isort', 'mkdocs', 'prospector', 'mypy', 'ipython', 'poetry', 'pytest', 'safety'
 foreach ($Package in $Packages) {
     pipx install $Package
 }
