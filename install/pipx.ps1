022d8b1f43bc3e079a3f1893eefa9c279407a74b
Daniel Levita <daniel.re4@gmail.com>
2022-11-14 16:17:18 -0300

    update packages

--- install/pipx.ps1
+++ install/pipx.ps1
@@ -3,7 +3,7 @@ Set-Location $env:USERPROFILE/AppData/Roaming/Python/Python310/Scripts
 ./pipx ensurepath
 
 # Install packages
-$Packages = 'blue', 'isort', 'mkdocs', 'prospector', 'mypy', 'ipython', 'poetry', 'pytest', 'safety', 'cookiecutter'
+$Packages = 'blue', 'isort', 'prospector', 'mypy', 'sphinx', 'ipython', 'pytest', 'safety', 'cookiecutter', 'conda-lock'
 foreach ($Package in $Packages) {
     pipx install $Package
 }
