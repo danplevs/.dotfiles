922f888c749b97663cb84837318689564a9889ad
Daniel Levita <daniel.re4@gmail.com>
2023-03-23 11:50:57 -0300

    Split packages installation

--- install/pipx.ps1
+++ install/pipx.ps1
@@ -1,9 +1,3 @@
 python -m pip install --user pipx
 Set-Location $env:USERPROFILE/AppData/Roaming/Python/Python311/Scripts
 ./pipx ensurepath
-
-# Install packages
-$Packages = 'blue', 'isort', 'prospector', 'ipython', 'pytest', 'safety', 'cookiecutter', 'conda-lock'
-foreach ($Package in $Packages) {
-    pipx install $Package
-}
