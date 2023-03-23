30624a69743a0451d7f47b469461eef0f613a47f
Daniel Levita <daniel.re4@gmail.com>
2023-03-23 16:46:29 -0300

    Update pipx.ps1

--- install/pipx.ps1
+++ install/pipx.ps1
@@ -2,9 +2,6 @@ python -m pip install --user pipx
 Set-Location $env:USERPROFILE/AppData/Roaming/Python/Python311/Scripts
 ./pipx ensurepath
 
-# restart terminal
-pwsh
-
 # Install packages
 $Packages = 'poetry', 'blue', 'isort', 'prospector', 'ipython', 'pytest', 'safety', 'cookiecutter', 'conda-lock'
 foreach ($Package in $Packages) {
