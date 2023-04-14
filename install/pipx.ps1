a29b8ca848dbc24de5b873e878e8e5f4f7aea37c
Daniel Levita <71600818+danplevs@users.noreply.github.com>
2023-04-13 22:32:39 -0300

    add poetry

--- install/pipx.ps1
+++ install/pipx.ps1
@@ -1,9 +1,8 @@
 python -m pip install --user pipx
-Set-Location $env:USERPROFILE/AppData/Roaming/Python/Python310/Scripts
-./pipx ensurepath
+pipx ensurepath
 
 # Install packages
-$Packages = 'blue', 'isort', 'prospector', 'ipython', 'pytest', 'safety', 'cookiecutter', 'conda-lock'
+$Packages = 'poetry', 'blue', 'isort', 'prospector', 'ipython', 'pytest', 'safety', 'cookiecutter', 'conda-lock'
 foreach ($Package in $Packages) {
     pipx install $Package
 }
