2388df286eb7fc76e98b56b641a7f77c7504a796
Daniel Levita <daniel.re4@gmail.com>
2023-07-21 16:37:53 -0300

    Update pipx.ps1

--- install/pipx.ps1
+++ install/pipx.ps1
@@ -2,7 +2,7 @@ python -m pip install --user pipx
 pipx ensurepath
 
 # Install packages
-$Packages = 'poetry', 'blue', 'isort', 'prospector', 'ipython', 'pytest', 'safety', 'cookiecutter', 'conda-lock'
+$Packages = 'poetry', 'blue', 'isort', 'prospector', 'ipython', 'pytest', 'safety', 'cookiecutter', 'conda-lock', 'pyinstaller'
 foreach ($Package in $Packages) {
     pipx install $Package
 }
