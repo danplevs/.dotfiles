da8a39236e75740305dafc8c109961718b3c984f
Daniel Levita <71600818+danplevs@users.noreply.github.com>
2023-07-25 09:46:23 -0300

    clean pwsh profile

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -13,9 +13,6 @@ $omp_config = Join-Path $PSScriptRoot "night-owl.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 $env:POSH_GIT_ENABLED = $true
 
-# Conda
-# (& "~/miniconda3/Scripts/conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
-
 # Scoop
 $env:PATH += ";$env:USERPROFILE\scoop\shims"
 $env:PSModulePath += ";$env:USERPROFILE\scoop\modules"
@@ -23,11 +20,6 @@ $env:PSModulePath += ";$env:USERPROFILE\scoop\modules"
 # Disable pwsh venv
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 
-# Chocolatey
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
 
 # PSReadLine
 Set-PSReadLineOption -PredictionViewStyle ListView
@@ -41,19 +33,19 @@ Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory
 
 # Env
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
-$env:PATH="$env:PATH;C:\Users\daniel\AppData\Roaming\Python\Scripts"
+$env:PATH += ";$env:USERPROFILE\AppData\Roaming\Python\Scripts"
 
 # Alias
 Set-Alias sudo gsudo
-Set-Alias radian "~/miniconda3/envs/radian/Scripts/radian.exe"
-Set-Alias r radian
 Set-Alias vim nvim
 Set-Alias ll ls
-Set-Alias g git
 Set-Alias grep findstr
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 
-# Utilities
+# Utils
+$env:PATH += ";$env:USERPROFILE\.dotfiles\install"
+$env:PATH += ";$env:USERPROFILE\.dotfiles\utils"
+
 function which ($command) {
   Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
